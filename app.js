5698baa8d876181c00ede8830c30dbc03928300a
Wesam20061 <144031435+Wesam20061@users.noreply.github.com>
2023-10-16 17:17:20 +0200

    Add files via upload

---
+++ app.js
@@ -0,0 +1,25 @@
+function Calculate(getal1,getal2)
+{
+    antwoorrd = getal1 + getal2;
+    console.log("Het antwoord van de som is: ");
+    console.log(antwoord);
+
+}
+    
+function Keer5(getal1)
+{
+    antwoorrd = getal1 * 5;
+    console.log("Het antwoord van de som is: ");
+    console.log(antwoord);
+
+}
+
+function calculateMinutes(seconden)
+{
+    antwoord = seconden / 60;
+    console.log("Het antwoord van de som is: ");
+    console.log(antwoord);
+
+}
+
+  
